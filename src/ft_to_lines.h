904c0bf42ef4a13d489da5f8a248f6b36b6b512f
맹주영 <mjy9088@naver.com>
2022-06-15 01:23:34 +0900

    add prototype

--- src/ft_to_lines.h
+++ src/ft_to_lines.h
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 22:18:13 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/06/14 00:50:21 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/06/15 01:23:18 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,67 @@ typedef struct s_ft_to_lines {
 	size_t			buffer_size;
 }	t_ft_to_lines;
 
-t_err	ft_to_lines(
+/**
+ * @brief initialize ft_to_lines context
+ *
+ * @param context address to store context
+ * @param buffer_size new buffer size
+ */
+void	ft_to_lines_init(
+			t_ft_to_lines *context,
+			size_t buffer_size);
+
+/**
+ * @brief get line from context
+ *
+ * @param context data contains previous read data
+ * @param out_line out parameter to get result
+ * @param out_line_length required if the line contains null characters
+ * @return true on error
+ *
+ * @remarks all data is drained when not error and (*out_line) is NULL
+ * @see the data can be added by ft_to_lines_feed
+ */
+t_err	ft_to_lines_drain(
+			t_ft_to_lines *context,
+			char **out_line,
+			size_t *out_line_length);
+
+/**
+ * @brief add temporary data to context, when no temporary data remains
+ *
+ * @param context data contains previous read data
+ * @param append address of temporary data, maybe from read()
+ * @param append_length length of append
+ * @return true on error (error if data not drained before call)
+ *
+ * @warning append MUST NOT FREED before drain all data by ft_to_lines_drain
+ * @see ft_to_lines_drain
+ */
+t_err	ft_to_lines_feed(
 			t_ft_to_lines *context,
 			const void *append,
-			size_t append_length,
-			char **out_line);
+			size_t append_length);
 
-char	*ft_to_lines_end(t_ft_to_lines *context);
-void	ft_to_lines_end_without_last_line(t_ft_to_lines *context);
+/**
+ * @brief get current *incomplete* line (may useful to get last line)
+ *
+ * @param context data contains previous read data
+ * @param out_line out parameter to get result, only if not NULL
+ * @param out_line_length required if the line contains null characters
+ * @return true on error
+ */
+t_err	ft_to_lines_get_current_line(
+			t_ft_to_lines *context,
+			char **out_line,
+			size_t *out_line_length);
+
+/**
+ * @brief release all resources associated with context
+ *
+ * @param context data contains previous read data
+ */
+void	ft_to_lines_free(
+			t_ft_to_lines *context);
 
 #endif
